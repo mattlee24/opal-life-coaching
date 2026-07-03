2b1d8e6a0b2f035e2171feab78e7febc97534c25
Matt Lee <matthew.d.lee@hotmail.co.uk>
2026-07-03 18:29:53 +0100

    Add Opal logo mark as the site favicon.
    Generate favicon and apple-touch-icon assets from logo-mark.png and link them in the document head.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
     >
       <Head>
         <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body className={manrope.className}>
         <Main />
